f1050733ccb897131cfb865d772d736263f5fb28
tonipcv <xppsalvador@gmail.com>
2025-05-10 20:37:52 -0300

    sucesso

--- src/app/twitter-image.js
+++ src/app/twitter-image.js
@@ -1,3 +1,5 @@
+import { ImageResponse } from 'next/og';
+
 // Use the same image for Twitter as for OpenGraph
 export { default } from './opengraph-image';
 export const alt = 'MED1 - Be the TOP1';
